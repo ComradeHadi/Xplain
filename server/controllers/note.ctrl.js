fbb030b6377f7218e23c9cab4f9221a6cbbba060
unknown <comradehadi@gmail.com>
2019-01-12 09:31:09 +0000

    initial commit

---
+++ server/controllers/note.ctrl.js
@@ -0,0 +1,36 @@
+
+const Note = require('./../models/Note.js')
+const Student = require('./../models/Student.js')
+const fs = require('fs')
+const cloudinary = require('cloudinary')
+
+module.exports = {
+
+
+	createNote: ()=>{
+
+	},
+
+	getNote: ()=>{
+
+	},
+
+	editNote: ()=>{
+
+	},
+
+	deleteNote: ()=>{
+
+	},
+
+	upvoteNote: ()=>{
+
+	}
+	,
+
+	recommendNote: ()=>
+	{
+
+		
+	}
+}
